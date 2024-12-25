09e761fd21c75b962f9e8c3a337b9aa980db6d27
Jeeva <jeevat13@gmail.com>
2024-12-25 08:59:37 +0530

    optimisations

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,25 @@ import Link from "next/link";
 import ProductCart from "./components/ProductCart";
 import { getServerSession } from "next-auth";
 import { providerOptions } from "./api/auth/[...nextauth]/route";
+import tree from "@/public/images/trees.jpg";
 
 export default async function Home() {
   const session = await getServerSession(providerOptions);
 
   return (
     <main>
-      <h1>Hello {session ? session.user?.name : "World!"}</h1>
-      <ProductCart />
+      <div className="relative h-screen">
+        <div>
+          <Image
+            src="https://shorturl.at/zjFZW"
+            alt="lake"
+            className="object-cover"
+            sizes="100vw"
+            quality={100}
+            fill
+          />
+        </div>
+      </div>
     </main>
   );
 }
